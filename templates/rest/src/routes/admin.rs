c24c32d6173f66f832abb54200e621ef94574cd4
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-14 16:52:36 -0300

    Init routes

---
+++ templates/rest/src/routes/admin.rs
@@ -0,0 +1,15 @@
+use actix_web::{
+    web::{delete, get, post, put, scope},
+    Scope,
+};
+
+use crate::handlers::admin::{create_user, delete_user, get_user, get_users, update_user};
+
+pub fn user_routes() -> Scope {
+    scope("/admin/users")
+        .route("/", get().to(get_users))
+        .route("/", post().to(create_user))
+        .route("/{id}", get().to(get_user))
+        .route("/{id}", put().to(update_user))
+        .route("/{id}", delete().to(delete_user))
+}
